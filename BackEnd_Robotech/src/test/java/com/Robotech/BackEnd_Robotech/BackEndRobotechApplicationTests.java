6116eab322f2ef3c1dfa7bcf24b98c2312434121
xtheuchy <elviscayllahuacondori@gmail.com>
2025-10-27 13:36:45 -0500

    archivo back

---
+++ BackEnd_Robotech/src/test/java/com/Robotech/BackEnd_Robotech/BackEndRobotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Robotech.BackEnd_Robotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndRobotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
